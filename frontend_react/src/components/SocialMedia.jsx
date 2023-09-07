f6b6223ab1376f8b5a9f3be3883aef9f7f41f56b
Grey Boolean <greyboolean@gmail.com>
2023-09-07 18:45:49 +0530

    Modified SocialMedia component

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { FaHackerrank } from 'react-icons/fa';
 
 const SocialMedia = () => (
-  <div className="app__social">
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
-    </div>
-  </div>
+	<div className="app__social">
+		<div>
+			<a
+				href="https://www.linkedin.com/in/madukasaubhagya/"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<BsLinkedin />
+			</a>
+		</div>
+		<div>
+			<a
+				href="https://www.hackerrank.com/greyboolean"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<BsGithub />
+			</a>
+		</div>
+		<div>
+			<a
+				href="https://github.com/greyboolean"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<FaHackerrank />
+			</a>
+		</div>
+	</div>
 );
 
 export default SocialMedia;
