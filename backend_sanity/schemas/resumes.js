f3786a1c2260904befe22e2ee2480685e076aa57
Grey Boolean <greyboolean@gmail.com>
2023-09-18 17:02:42 +0530

    Title field added to the Resume schema

--- backend_sanity/schemas/resumes.js
+++ backend_sanity/schemas/resumes.js
@@ -4,6 +4,11 @@ export default {
 	type: 'document',
 	fields: [
 		{
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+		},
+		{
 			name: 'resumeFile',
 			title: 'Resume File',
 			type: 'file',
